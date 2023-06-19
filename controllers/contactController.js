8022d1305ed83c66045d805e67e42bed33fc3218
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-19 15:31:29 +0100

    contact controller modified(get, update, delete by id added)

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,3 +5,35 @@
 exports.getAllContacts = (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
 };
+
+//@desc Create a contact
+//@route POST /api/contacts
+//@public
+
+exports.createContact = (req, res) => {
+  res.status(201).json({ message: "Create a new contact" });
+};
+
+//@desc Get one contact
+//@route GET /api/contacts/:id
+//@public
+
+exports.getContactById = (req, res) => {
+  res.status(200).json({ message: "Get one contact by id" });
+};
+
+//@desc Update one contact
+//@route PUT /api/contacts/:id
+//@public
+
+exports.updateContactById = (req, res) => {
+  res.status(201).json({ message: "Update one contact by id" });
+};
+
+//@desc Delete one contact
+//@route DELETE /api/contacts/:id
+//@public
+
+exports.deleteContactById = (req, res) => {
+  res.status(204).json({ message: "Delete one contact by id" });
+};
--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,21 +1,12 @@
 const express = require("express");
+const contactController = require("../controllers/contactController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
-});
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Create a new contact" });
-});
-router.get("/:id", (req, res) => {
-  res.status(200).json({ message: "Get one contact by id" });
-});
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: "Update one contact by id" });
-});
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: "Delete one contact by id" });
-});
+router.get("/", contactController.getAllContacts);
+router.post("/", contactController.createContact);
+router.get("/:id", contactController.getAllContacts);
+router.put("/:id", contactController.updateContactById);
+router.delete("/:id", contactController.deleteContactById);
 
 module.exports = router;
