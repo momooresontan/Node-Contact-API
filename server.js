dd4963abf4a35e2e7df72753ea7bed2cdf98142a
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-19 14:47:49 +0100

    server.js modified

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
