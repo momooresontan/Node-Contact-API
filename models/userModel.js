18d3df8148073c5f536ec1b8442de63dc859f4e3
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-20 18:37:11 +0100

    user model modified

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,18 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please enter a user name"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter user email address"],
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: [true, "Please enter user phone number"],
+  },
+});
