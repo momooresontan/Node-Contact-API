b131762d5fa253723ff6d884d00142fa19d631a2
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-23 03:14:34 +0100

    contact controller modified

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -40,6 +40,14 @@ exports.getContactById = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact not found!");
   }
+
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error(
+      "User does not have permission to get other user's contacts!"
+    );
+  }
+
   res.status(200).json(contact);
 });
 
